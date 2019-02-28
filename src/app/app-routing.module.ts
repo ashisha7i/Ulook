ea2a5ac4fc2b26de3776c97f89235a56655ff630
Ashish Agnihotri <aagnihot@cisco.com>
2019-02-27 17:18:54 -0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { AboutComponent } from './component/about/about.component';
+import { HomeComponent } from './component/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
